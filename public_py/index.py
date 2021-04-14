377d9e42a0aefb6a748d5656ca28ae7d10ae28db
Johann Ganzert <johannganzert@yahoo.com.br>
2021-04-14 17:26:54 -0300

    testando com while e mais codigos

--- public_py/index.py
+++ public_py/index.py
@@ -1,1 +1,13 @@
-testando o commit
+
+n = int(input('Digite um valor: '))
+
+soma = 0
+
+while True:
+    while n >= 0:
+        if n / 3 == 0 or n / 5 == 0:
+            soma = soma + n
+            print(soma, end='->')
+        n = n - 1
+    if n == 0:
+        break
